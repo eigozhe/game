86110600f8f8b03df71b20d96ca8214e2d797cb0
eigozhe <70949616+eigozhe@users.noreply.github.com>
2026-02-19 01:06:06 +0200

    Add files via upload

--- game.py
+++ game.py
@@ -1,74 +1,51 @@
 # создать разные типы оружия и сделать возможность менять их вне боя (во время боя их переключать нелья а если хочется то тратиться действие) также чтобы характеристики оружия либо менялись в статах персонажа или (по возможности) подключать их отдельно в качестве отдельного масива
 from random import randint
 
-steve = {'atack':10,'strongAtack':25,'stamina':100,'rest':20,'hp':100,'chanceAtack':(40,80),'missAtack':(0,39),'chanceSuperatack':(81,100)}
+steve = {'atack': 10,'strongAtack': 25,'stamina': 100,'rest': 20,'hp': 100,'chanceAtack': (40, 80),'missAtack': (0, 39),'chanceSuperatack': (81, 100),'weapon': 'ironSword'}
 
-creaper= {'atack':12,'hp':150,'chanceAtack':(40,100),'missAtack':(0,39),'chanceSuperatack':(101,101)}
+creaper = {'atack': 12,'hp': 150,'chanceAtack': (61, 100),'missAtack': (0, 60),'chanceSuperatack': (101, 101)}
 
-def inventoryy():
-    return {
-        'ironSword':10,
-        'battleAxe':15,
-        'assassinDagger':20,
-        'longbow':25
-    }
+inventory = {
+    1: ('ironSword', 10),
+    2: ('battleAxe', 15),
+    3: ('assassinDagger', 20),
+    4: ('longbow', 25)
+}
 
-inventory = inventoryy()
-steve['equippedWeapon'] = 'ironSword'
+
+def Inventory(choice, steve):
+    if choice in inventory:
+        steve['weapon'], steve['atack'] = inventory[choice]
+        print("You equipped", steve['weapon'])
+    else:
+        print("Wrong choice")
+
 
 def attack(atack, target):
     target = target - atack
-    print(target)
-
-attack(steve['atack'], creaper['hp'])
-attack(creaper['atack'], steve['hp'])
+    print("target hp:", target)
+    return target
 
 
-def staminaa(stamina, target):
-    stamina = stamina - target
-    print(stamina)
-
-attack(steve['atack'], steve['stamina'])
-attack(steve['strongAtack'], steve['stamina'])
+def staminaa(stamina, cost):
+    stamina = stamina - cost
+    print("stamina:", stamina)
+    return stamina
 
 
 def chance(chanceAtack, missAtack, chanceSuperatack):
     chancee = randint(1, 100)
-    print(chancee)
-
+    print("randint:", chancee)
     return ["miss", "attack", "strongAttack"][
-        (chancee >= chanceAtack[0]) +
-        (chancee >= chanceSuperatack[0])
+        (chancee >= chanceAtack[0]) + (chancee >= chanceSuperatack[0])
     ]
 
-result = chance(
-    steve['chanceAtack'],
-    steve['missAtack'],
-    steve['chanceSuperatack']
-)
-print(result)
-
-result = chance(
-    creaper['chanceAtack'],
-    creaper['missAtack'],
-    creaper['chanceSuperatack']
-)
-print(result)
 
 while steve['hp'] > 0 and creaper['hp'] > 0:
 
-    print(f"""
-____________________________________________________________
-| steve {steve['hp']} | Enemy {creaper['hp']}        |
-| stamina {steve['stamina']} | EnemyDamage {creaper['atack']} |
-| weapon {steve['equippedWeapon']}|
-------------------------------------------------------------
-| 1. attack                                              |
-| 2. time (+20 stamina)                                  |
-| 3. strong attack                                       |
-| 4. change weapon                                       |
-____________________________________________________________
-""")
+    print("1 - attack")
+    print("2 - strong attack")
+    print("3 - rest")
+    print("4 - inventory")
 
     game = int(input("what you wanna do: "))
-
